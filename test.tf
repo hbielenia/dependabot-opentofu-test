ccf0dd91fde10925cde5192eced19abce62a0349
Hubert Bielenia <hubert.bielenia@gmail.com>
2026-04-28 00:29:11 +0200

    Initial commit

---
+++ test.tf
@@ -0,0 +1,15 @@
+terraform {
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "6.42.0"
+		}
+	}
+}
+
+provider "aws" {
+	region = "us-east-2"
+}
+
+data "aws_caller_identity" "current" {}
+
